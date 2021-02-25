169948fd00d1ffb017606998b229ac1b4a46a1f3
wezaMingosso <71614675+wezaMingosso@users.noreply.github.com>
2021-02-25 06:04:16 -0300

    Update sytles.js
    atualizacao em styles de cadastro

---
+++ front-end/src/pages/CadastroEstufa/sytles.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const Conteiner= styled.div`
+    
+    background-color:green;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    min-height:calc(100vh - 55px);
+    
+   
+`
+
+export const ImageLogo = styled.img`
+    height: 100px;
+    width: 15%;
+`
+export const Title = styled.h1`
+    font-weight:bolder;
+    color:white;
+    font-size:x-large;
+    margin-bottom:1.5em;
+    margin-left:15px;
+    
+`
+export const Label = styled.label`
+    
+    color:white;
+  
+    `
+    
+    export const MeuBotao = styled.button`
+    background: green;
+    border: 2px solid ;
+    border-radius: 10px;
+    color: white;
+    font-size: 1.2rem;
+    margin: 4rem;
+    padding: 1rem 3rem;
+    `
