4f8c385e62a98f22b93ce56a45c48c6d221e5a38
wezaMingosso <71614675+wezaMingosso@users.noreply.github.com>
2021-02-27 13:44:42 -0300

    Add files via upload
    add adicionando cadastro de elementos

---
+++ front-end/src/pages/Cadastraelementos/styles.js
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+
+export const Conteiner= styled.div`
+    
+    background-color:green;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    min-height:calc(100vh - 55px);
+    
+   
+`
+
+export const ImageLogo = styled.img`
+    height: 100px;
+    width: 9%;
+`
+export const Title = styled.h1`
+   font-size: 60px;
+  text-align: center;
+  color:white;
+    
+    
+    
+`
+export const Title1 = styled.p`
+   font-size: 60px;
+  text-align: center;
+  color:white;
+    
+    
+    
+`
+export const Title2 = styled.p`
+    
+    color:white;
+    font-size:20px;
+    line-height:2;
+    letter-spacing:1px;
+    
+    
+    
+`
+export const Label = styled.label`
+    font-weight:bolder;
+    color:white;
+    font-size:1.2em;
+    margin-bottom:1%;
+    
+   
+    `
+    
+    export const Input = styled.input`
+    padding:5px;
+    display:flex;
+    justify-content:space-around;
+    border-radius:20em;
+    `
+
+    export const MeuBotao = styled.button`
+    background: green;
+    border: 2px solid ;
+    border-radius:20em;
+    color: white;
+    font-size: 1.2rem;
+    margin: 2rem;
+    padding: 1rem 3rem;
+    
+    `
