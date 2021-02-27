a6dcab8f3ab8a3b690bdb0e14a28b126f8e6f79f
João Paulo Stefani Fregni <45856037+JPFregni@users.noreply.github.com>
2021-02-27 18:53:25 -0300

    icones add EstufaAtiva
    adicionado icones na estufa ativa para teste

--- front-end/src/pages/EstufaAtiva/index.js
+++ front-end/src/pages/EstufaAtiva/index.js
@@ -4,6 +4,7 @@ import Header from '../../components/Header'
 import Title from '../../components/Title'
 import CardStatus from '../../components/cardDetalhe'
 import FotoHorta from '../../components/horta.jpg'
+import { faLightbulb, faFan, faWater } from '@fortawesome/free-solid-svg-icons'
 
 function EstufaAtiva() {
     return (
@@ -13,9 +14,9 @@ function EstufaAtiva() {
             <Title title="Estufa Jão" />
             <ImageHorta src={FotoHorta}/>
             <CardConteiner>
-                 <CardStatus cor="red" tipo="Banana" corIcon="red" iconess="" ativos="Ativo" quant="10"/>
-                 <CardStatus cor="red" tipo="Banana" corIcon="red" iconess="" ativos="Ativo" quant="10"/>
-                 <CardStatus cor="red" tipo="Banana" corIcon="red" iconess="" ativos="Ativo" quant="10"/>
+                 <CardStatus cor="red" tipo="Banana" corIcon="red" iconess={faLightbulb} ativos="Ativo" quant="10"/>
+                 <CardStatus cor="red" tipo="Banana" corIcon="red" iconess={faFan} ativos="Ativo" quant="10"/>
+                 <CardStatus cor="red" tipo="Banana" corIcon="red" iconess={faWater} ativos="Ativo" quant="10"/>
             </CardConteiner> 
         </Conteiner>
         </>
