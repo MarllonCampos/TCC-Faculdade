1c6d5e569c6c4fb03fd06d5af033247055edbf24
Marllon Campos <marllondcsp@gmail.com>
2021-03-02 01:04:25 -0300

    Colocado um botao padrao para o projeto

---
+++ front-end/src/components/Button/index.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+    width:80%;
+    height:45px;
+    
+    background-color:var(--secondary);
+    
+    border:0;
+    border-radius:600px;
+    
+    text-align:center;
+    color:var(--text);
+    font-family:Poppins, sans-serif;
+    font-weight: 600;
+    font-size:1.2rem;
+
+    cursor:pointer;
+`
+export default Button;
