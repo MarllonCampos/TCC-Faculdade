63dff757d676c3fbeff3775fc08748c5edb158d6
João Paulo Stefani Fregni <45856037+JPFregni@users.noreply.github.com>
2021-03-03 12:33:35 -0300

    alterações no card

--- front-end/src/components/card/index.jsx
+++ front-end/src/components/card/index.jsx
@@ -3,31 +3,44 @@ import logo from "../../assets/logo.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLightbulb, faFan, faWater } from '@fortawesome/free-solid-svg-icons'
 import './Card.css'
+import FotoHorta from '../../assets/horta.jpg'
 
 
 const Card = props => (
 
     <div class="ui card">
-        <div class="content">
-            <img class="image" src={logo}/>
+            <img class="image" src={FotoHorta}/>
+        <div class ="coluna">
             <p className="textoStatus">{props.nome}</p>
+            <div class="luz">
+                <FontAwesomeIcon icon={faLightbulb} /> 
+                <p class="onoff">Ativo</p> 
+            </div>
+            <div class="vento">
+                <FontAwesomeIcon icon={faFan} /> 
+                <p class="onoff">Ativo</p> 
+            </div>
+            <div class="agua">
+                <FontAwesomeIcon icon={faWater} /> 
+                <p class="onoff">Ativo</p> 
+            </div>
         </div>
+        
+        
 
-        <div class="content">
-            <FontAwesomeIcon icon={faLightbulb} />
-            <FontAwesomeIcon icon={faFan} />
-            <FontAwesomeIcon icon={faWater} />
+        {/* <div class="content">
+            
+            
+            
         </div>
         <div >
-            <button class="ui green button">Ativo</button>
+            
+            
             <button class="ui yellow button">Verificar</button>
             <button class="ui blue button">Em andamento</button>
 
             <p className="textData">Criado em {props.data}</p>
-        </div>
-
-      
-
+        </div> */}
     </div>
 
 )
