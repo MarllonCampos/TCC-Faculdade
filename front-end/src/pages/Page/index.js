f2d41fd6cb9958805fca1710f7d4d4d0d32a6b29
Marllon Campos <marllondcsp@gmail.com>
2021-03-03 18:58:24 -0300

    Trago de volta o botao padrao do projeto

--- front-end/src/pages/Page/index.js
+++ front-end/src/pages/Page/index.js
@@ -4,6 +4,9 @@ import Header from '../../components/Header'
 import InputText from '../../components/Input';
 import OlaLogo from '../../components/Ola';
 import Select from '../../components/Select'
+import Button from '../../components/Button'
+
+
 import { Conteiner } from '../EstufaAtiva/styles'
 
 
@@ -28,6 +31,8 @@ function Page({ title, color, border, ...props }) {
                 <InputText noIcon idFor="user" labelText="Usuário:" />
                 <Select labelText="Usuário: " onChange={setSelectState} label="select" elements={["Marllon Campos", 2, 3, 4, 5, 6, 7, 8, 9]} />
                 <InputText idFor="password" labelText="Senha:" />
+
+                <Button>Cadastrar</Button>
 
 
             </Conteiner>
