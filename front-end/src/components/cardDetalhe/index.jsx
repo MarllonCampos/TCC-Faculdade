cdce43c855c925dfe7ecaba72fce6a39878f6f09
João Paulo Stefani Fregni <45856037+JPFregni@users.noreply.github.com>
2021-03-05 15:42:10 -0300

    organizando o codigo

--- front-end/src/components/cardDetalhe/index.jsx
+++ front-end/src/components/cardDetalhe/index.jsx
@@ -3,22 +3,19 @@ import React from 'react'
 import "./CardDetalhe.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLightbulb, faFan, faWater } from '@fortawesome/free-solid-svg-icons'
-const element =<FontAwesomeIcon icon={faFan}/>
+const element = <FontAwesomeIcon icon={faFan} />
 
 
 const CardStatus = props => (
-       
-    
- <div class="ui card2" style={{backgroundColor: props.cor}}>
+
+
+    <div class="ui card2" style={{ backgroundColor: props.cor }}>
         <p className="textoTopo">{props.tipo}</p>
-        <div class="ui card2" id="icone1"  style={{backgroundColor: props.corIcon}}>
+        <div class="ui card2" id="icone1" style={{ backgroundColor: props.corIcon }}>
             <div>
                 <FontAwesomeIcon id="icone" className="icone" icon={props.iconess} />
             </div>
-            <h1 className="ativos">{props.ativos} Ativos
-
-
-                    </h1>
+            <h1 className="ativos">{props.ativos} Ativos</h1>
             <div className="instalado">
                 <p className="textoTopo">
                     Instalado
