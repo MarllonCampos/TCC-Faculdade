60a54dab3cb72b37e27a4af49f3acbb392133e83
RutyRibeiro <64114456+RutyRibeiro@users.noreply.github.com>
2021-03-09 23:43:30 -0300

    complementando rota login de usuário

--- backend/utils.py
+++ backend/utils.py
@@ -2,36 +2,33 @@ from validate_email import validate_email
 
 def validateLogin(user):
     try:
-        email = user['email']
-        password = user['password']
+        if 'email' in user and 'password' in user:
+            
+            email = user['email']
+            password = user['password']
 
-        if (email is None or email == ''):
-           
-            print ('Email não informado! ')
+            if (email is None or email == ''):
+                raise Exception('Email não informado!')
             
-            raise Exception('Email não informado!')
+            elif not(validate_email(email_address = email, check_regex = True, check_mx = False)):
+                raise Exception('E-mail não valido!')
         
-        elif (validate_email(email_address = email, check_regex = True, check_mx = False)):
+            elif (password is None or password == '') :
+                raise Exception('Senha não informado!')
             
-            print ('E-mail não valido!')
-            
-            raise Exception('E-mail não valido!')
-        
-        elif (password is None or password == '') :
-            
-            print ('Senha não informado!')
-            
-            raise Exception('Senha não informado!')
-     
+            else:
+                return({
+                    "status":"ok",
+                    "message":"validado",
+                    "user":{
+                        "email": email,
+                        "password": password
+                        }
+                    })
         else:
-            return({
-                "status":"sucesso",
-                "message":"validado",
-                "user":{
-                    "email": email,
-                    "password": password
-                    }
-                })
+            raise Exception('E-mail ou senha inexistente!')
 
-    except Exception as e:
-        return({"status":"erro","message":e})
+    except Exception as error:
+        
+        response = {"status":"erro","message":str(error)}
+        return(response)
