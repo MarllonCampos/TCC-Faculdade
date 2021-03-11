3a3e3ffa3ece1aeb74570a46a5c114dee488cac8
RutyRibeiro <64114456+RutyRibeiro@users.noreply.github.com>
2021-03-11 00:22:31 -0300

    Criando função para trazer informações das estufas de cada usuário

--- backend/bd_functions.py
+++ backend/bd_functions.py
@@ -15,7 +15,6 @@ def login(user):
 
         conn = openConnection()
        
-
         if type(conn) == dict:
             return conn
 
@@ -23,10 +22,7 @@ def login(user):
             try:
                 cursor = conn.cursor()
 
-                # searchLogin = f"call loginTeste('{email}')"
                 cursor.callproc('loginTeste', [email,])
-
-                result = cursor.stored_results()
                 
                 for result in cursor.stored_results():
                     row = result.fetchall()
@@ -34,30 +30,78 @@ def login(user):
                 cursor.close()
                 
                 if len(row) == 0:
-                    return({'status':'erro', 'message': 'Email ou senha incorreto(s)!'})
+                    raise Exception('Email ou senha incorreto(s)!')
                 
                 else:
+                    idUser = row[0][0]
+                    nomeUser = row[0][1]
                     passwordBD = row[0][3]
+                    # criptografando senha usando o formato md5
                     passwordEncode = hashlib.md5(password.encode())
-                    print(passwordEncode)
-                    print(passwordBD)
 
                     if passwordEncode.hexdigest() == passwordBD:
-                        return({
-                            "status":"ok",
-                            "message":"validado",
-                            "user":{
-                                "email": email,
-                                "password": password
-                                }
-                            })
+                        response = getUser(idUser, nomeUser)
+                        return response
 
                     else:
-                        return({'status':'erro', 'message': 'Email ou senha incorreto(s)!'})
-            
+                        raise Exception('Email ou senha incorreto(s)!')
+
             except Exception as err:
-                return({'status':'erro', 'message': str(err)})
+                return({'message':{'title':'Erro',
+                'content': str(err)},
+                'status':'erro'})
+            
             finally:
+                conn.commit()
+                conn.close()
+
+#Função que traz informações relacionadas ao usuário depois de validado o login 
+def getUser(idUser,nomeUser):
+    conn = openConnection()
+
+    if type(conn) == dict:
+            return conn
+    else:
+        try:
+            cursor = conn.cursor()
+
+            cursor.callproc('getGreenerys',[idUser])
+            data=[]
+            
+            for result in cursor.stored_results():
+                row = result.fetchall()
+                for greenery in row:
+                    idestufa = greenery[0]
+                    greenery={"idestufa": greenery[0],
+                            "nomeestufa": greenery[1],
+                            "ativo": greenery[2],
+                            "dataestufa": greenery[3],
+                            "fotoestufa": greenery[4],
+                    }
+                    cursor.callproc('getElements',[idestufa])
+                    elements = []
+                    for element in cursor.stored_results():
+                        row = element.fetchall()
+                        for e in row:
+                            elem={"nomeelem": e[0],
+                            "ativo":e[1]
+                            }
+                            elements.append(elem)
+                    greenery['elementos'] = elements
+                    data.append(greenery)
+            
+            cursor.close()
+            return({
+                    "status":"ok",
+                    "user": nomeUser,
+                    "greenerys": data
+                    })
+        
+        except Exception as err:
+                return({'message':{'title':'Erro',
+                'content': str(err)},
+                'status':'erro'})
+        finally:
                 conn.commit()
                 conn.close()
 
