31e450f5c5c6132f0f48906986635dc44f4fe618
João Paulo Stefani Fregni <45856037+JPFregni@users.noreply.github.com>
2021-03-16 19:03:01 -0300

    testes

--- front-end/src/pages/ListEstufas/index.js
+++ front-end/src/pages/ListEstufas/index.js
@@ -8,7 +8,10 @@ function ListEstufas() {
         <Header icon />
         <Conteiner>
             <Card title="Estufa Jão" status={true} date="03/03/2021"/>
-            <Card title="Estufa Marllon" status={false} date="03/03/2021"/>  
+            <Card title="Estufa Marllon" status={false} date="06/03/2021"/>  
+            <Card title="Estufa Ruty" status={true} date="09/03/2021"/>  
+            <Card title="Estufa Rafa" status={false} date="15/03/2021"/>  
+            <Card title="Estufa Weza" status={true} date="16/03/2021"/>  
         </Conteiner>
         </>
     )
