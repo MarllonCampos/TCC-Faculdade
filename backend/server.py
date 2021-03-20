0736d71cf9b85191682ca758041334a4780fc583
RutyRibeiro <64114456+RutyRibeiro@users.noreply.github.com>
2021-03-19 21:07:07 -0300

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -4,6 +4,7 @@ from flask_cors import CORS, cross_origin
 import cv2
 import os
 app = Flask('Greenery')
+port = int(os.environ.get('PORT', 33507))
 cors = CORS(app, resources={r"/": {"origins": "*"}})
 
 @app.route('/login', methods=['POST'])
@@ -43,7 +44,7 @@ def delete():
 
     return ('DELETE')
 
-app.run(host='0.0.0.0')
+app.run(port=port, host='0.0.0.0')
 
 
 
