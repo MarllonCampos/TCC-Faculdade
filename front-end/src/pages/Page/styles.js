5301bd17d3df092ecdc97be15851838c1b55453f
Marllon Campos <marllondcsp@gmail.com>
2021-03-24 01:03:28 -0300

    Criado um sistema de grid para linkar outras paginas, e criado um fake Login

--- front-end/src/pages/Page/styles.js
+++ front-end/src/pages/Page/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-
+import {Link} from 'react-router-dom';
 export const TituloCima = styled.h1`
     color:green;
     font-size:20px;
@@ -19,3 +19,33 @@ export const Wrapper = styled.div`
     background-color: green;
     width:100%;
 `
+
+export const GridContainer = styled.div`
+    width:80vw;
+    display:grid;
+    grid-template-columns:auto;
+    margin:23px auto;
+    justify-items:center;
+    grid-gap:35px;
+
+`;
+export const GridItem = styled(Link)`
+    color:white;
+    background-color:rgba(6,71,17,56%);
+    text-align:center;
+    width:100%;
+    height:35px;
+    line-height:35px;
+    border-radius:7px;
+
+
+
+    &:hover {
+        background-color:rgba(6,71,17,86%);
+
+    }
+
+`
+
+
+
