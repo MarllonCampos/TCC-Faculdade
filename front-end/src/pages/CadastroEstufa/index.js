56fded056415b63322b19548e031d142a3caa057
wezaMingosso <71614675+wezaMingosso@users.noreply.github.com>
2021-03-26 04:45:15 -0300

    adcionando onclick e onchange

--- front-end/src/pages/CadastroEstufa/index.js
+++ front-end/src/pages/CadastroEstufa/index.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import { Conteiner,Title1 } from "./styles"
+import { Conteiner,Title1, } from "./styles"
+
 import Header from '../../components/Header'
 
 import Ola from '../../components/Ola'
 import InputText from '../../components/Input';
 import Button from '../../components/Button';
+import Photo from '../../components/Photo';
+
+
 function CadastroEstufa() {
-    
+
+  
+  function handlechange (event) {
+  console.log(event.target.value);
+}
+  function clickHandler(){
+    console.log('Button')
+  }      
     return (
         <>
         <Header icon />
         <Conteiner>
           <Ola/> 
         <Title1>Cadastro de Estufas </Title1>
-        <InputText noIcon idFor="user" labelText="Nome da Estufa:" /> 
-        <Button>Cadastrar</Button> 
+        <Photo  ></Photo>
+        <InputText noIcon idFor="user" labelText="Nome da Estufa:"  onChange={handlechange}/> 
+        
+        <Button onClick={clickHandler}>Cadastrar</Button> 
+        
         </Conteiner>
         </>
         
