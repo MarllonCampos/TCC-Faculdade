c1c097ca185a63834f5662544706199a68bf3882
rafael_bispo23@hotmail.com <rafael_bispo23@hotmail.com>
2021-03-27 14:24:58 -0300

    Galeria e Photo funcionando

--- front-end/src/pages/LoginEstufa/styles.js
+++ front-end/src/pages/LoginEstufa/styles.js
@@ -9,10 +9,25 @@ export const Conteiner = styled.div`
     justify-content: center;
     margin-bottom: 30px;
 
+    .input[type='file'] {
+  display: none
+}
+
+/* Aparência que terá o seletor de arquivo */
+.label {
+  background-color: #3498db;
+  border-radius: 5px;
+  color: #fff;
+  cursor: pointer;
+  margin: 10px;
+  padding: 6px 20px
+}
+
     
     
 
 `;
+
 
 export const Label = styled.label`
   display:flex;
@@ -24,6 +39,40 @@ export const Label = styled.label`
  
   
 `;
+export const LabelInput = styled.label`
+   display:block;
+    width:80%;
+    max-width:300px;
+    height:45px;
+    padding: 9px;
+    margin:0 auto;
+    margin-top:12px;
+    background-color:var(--secondary);
+    
+    border:0;
+    border-radius:600px;
+    
+    
+    text-align:center;
+    color:var(--text);
+    font-family:Poppins, sans-serif;
+    font-weight: 600;
+    font-size:1.2rem;
+
+    cursor:pointer;
+
+
+    &:disabled {
+        filter: brightness(0.55);
+        &:hover {
+            cursor:not-allowed;
+        }
+    }
+   
+ 
+  
+`;
+
 
 export const A = styled.a`
 
