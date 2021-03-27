b98c5dd3efbb4e56ee949602e499a0b2701a08df
rafael_bispo23@hotmail.com <rafael_bispo23@hotmail.com>
2021-03-27 14:24:58 -0300

    Galeria e Photo funcionando

--- front-end/src/pages/LoginEstufa/Uploader.js
+++ front-end/src/pages/LoginEstufa/Uploader.js
@@ -1,60 +1,84 @@
 import React, { useState } from 'react'
-import { BoxUpload,  Label } from '../../components/Photo/styles';
-import { A } from "./styles"
+import { BoxUpload, Label } from '../../components/Photo/styles';
+import { A, LabelInput } from "./styles"
 import Button from '../../components/Button'
 import Title from '../../components/Title'
 import Camera from '../../assets/Camera.png'
 import Form from '../../components/Form'
 import Main from '../../components/Main';
+
 import Ola from '../../components/Ola'
-import ComponentModal from '../../components/Photo/Galeria/ComponentModal'
-import Cameraa from '../../components/Photo/Cameraa'
+import Photo from '../../components/Photo';
+
 
 
 
 function Uploader(props) {
   const [isModalVisible, setIsModalVisible] = useState(false);
- 
+  const [image, setImage] = useState(Camera);
+  const [isUploaded, setIsUploaded] = useState(false);
+  const [typeFile, setTypeFile] = useState("");
+  function handleImageChange(e) {
+    
+
+    if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
+      // ok, o navegador tem suporte
+    }
+    if (e.target.files && e.target.files[0]) {
+      setTypeFile(e.target.files[0].type);
+      let reader = new FileReader();
+
+      reader.onload = function (e) {
+        setImage(e.target.result);
+        setIsUploaded(true);
+      };
+
+      reader.readAsDataURL(e.target.files[0]);
+    }
+
+  }
   return (
     <Main>
       <Form>
         <Ola />
         <Title title="Faça o reconhecimento facial" />
-        <BoxUpload  onClick={() => setIsModalVisible(true)}>
-          <div style={{cursor: "pointer"}}>          
-              
-                <Label style={{cursor: "pointer"}} >
-                  <img
-                  
-                    src={Camera}
-                    draggable={"false"}
-                    alt="placeholder"
-                    style={{ width: "80%", height: "100%",cursor: "pointer" }}
-                  />
-                  <p style={{ color: "#444", textAlign: "center"  }}>
-                    Click para Cadastra uma Imagem
-                  </p>
-                </Label>           
-            
+        <BoxUpload onClick={() => setIsModalVisible(true)}>
+          <div style={{ cursor: "pointer" }}>
+
+            <Label style={{ cursor: "pointer" }} >
+            <img
+               
+                src={image}
+                draggable={"false"}
+                alt="placeholder"
+                style={{ width: "80%", height: "100%", cursor: "pointer" }}
+              />
+             
+              <p style={{ color: "#444", textAlign: "center" }}>
+                Click para tirar uma foto
+              </p>
+            </Label>
+
           </div>
         </BoxUpload>
         {isModalVisible ? (
-      <ComponentModal onClose={() => setIsModalVisible(false)}>
-        <Cameraa/>
-      </ComponentModal>
-       ) : null}
+          <Photo/>
+        ) : null}
+        <div >
+          
 
-   
-              
-        
-        
-        
-        <div className="col-md-6 offset-md-3">
-          <div >
+<LabelInput for='selecao-arquivo'>Selecionar um arquivo</LabelInput>
+          <input style={{display:'none'} }
+                   id='selecao-arquivo'
+                    type="file"
+                    accept=".jpg,.jpeg,.gif,.png,.mov,.mp4"
+                    onChange={handleImageChange}
+                  />
+
             <Button><A href="/login-estufas">Cadastrar</A></Button>
-
+           
           </div>
-        </div>
+        
 
 
       </Form>
