10fb8016edeb32870dc7a600e9478d232326493c
wezaMingosso <71614675+wezaMingosso@users.noreply.github.com>
2021-04-02 01:14:32 -0300

    Update index.js
    atualizando select

--- front-end/src/pages/Cadastraelementos/index.js
+++ front-end/src/pages/Cadastraelementos/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect, useState } from "react";
+
 import { Conteiner,Title1,Title2, } from "./styles"
 import Header from '../../components/Header'
 import Select from '../../components/Select'
@@ -7,8 +8,37 @@ import InputText from '../../components/Input';
 import Button from '../../components/Button';
 
 
-function    CadastroElemento() {
+
+
+function  CadastroElemento() {
+    const [selectState, setSelectState] = useState(false)
+
+    useEffect(() => {
+        if (selectState != false) {
+            console.log(selectState.target.value)
+
+        }
+    }, [selectState])
+ 
+        
     
+
+       const [dados, setDados] = useState({nome:''})
+
+  
+     const handleInputChange = (event) => {
+          console.log(event.target.name)
+           console.log(event.target.value)
+          setDados({
+              ...dados,
+              [event.target.name] : event.target.value
+          })
+      }
+  
+      const enviarDados = (event) => {
+        event.preventDefault()
+        console.log('enviando dados...' + dados.nome+''+ selectState+'' )
+    }
     return (
         <>
         <Header icon/>
@@ -19,14 +49,18 @@ function    CadastroElemento() {
             <Title1>Cadastro de Estufas </Title1>
             <Title2>Elementos da Estufa</Title2>
            
-            <InputText noIcon idFor="user" labelText="Nome do Elemento:" />
-            <Select  selected disabled="Seleciona uma opcao" labelText="Tipo de elemento:" elements={[,"Lampada", 3, 4, 5, 6, 7, 8, 9]} />
-
-            
-            <Button>Cadastrar</Button>
+            <InputText noIcon idFor="user" placeholder="Digite o nome do Elemento"
+             labelText="Nome do Elemento:" onChange={handleInputChange} name="nome" />
+            <Select   labelText="Tipo de elemento:"  
+             elements={["Lampada", "Ventilador","Agua"]} 
+             onChange={setSelectState} label="select"
+             />
+             
+            <Button onClick={enviarDados} >Cadastrar</Button>
         </Conteiner>
         
         </>
     )
-     }
- export default CadastroElemento;
+    
+}
+export default CadastroElemento;
