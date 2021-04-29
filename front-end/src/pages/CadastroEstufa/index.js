45a9571a845c2c20a570f53a2ebf35cd07d30b79
João Paulo Stefani Fregni <45856037+JPFregni@users.noreply.github.com>
2021-04-29 16:53:21 -0300

    fix no cadastroestufa
    importando o modal

--- front-end/src/pages/CadastroEstufa/index.js
+++ front-end/src/pages/CadastroEstufa/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { Conteiner,Title1, } from "./styles"
+import { Conteiner, Title1, } from "./styles"
 
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
@@ -10,52 +10,53 @@ import Form from '../../components/Form';
 import Ola from '../../components/Ola'
 import InputText from '../../components/Input';
 import Button from '../../components/Button';
+import Modal from '../../components/Modal';
 
 
 
 function CadastroEstufa() {
 
-    const schema= yup.object().shape({
-    nomeestufa: yup.string().min(2,"Digite um nome para Estufa").required(),
+    const schema = yup.object().shape({
+        nomeestufa: yup.string().min(2, "Digite um nome para Estufa").required(),
     });
-    const {register, handleSubmit, errors}= useForm({
-        resolver:yupResolver(schema),
-        });
-        const newUser=(user)=> {
-            console.log(user);
-        };
-        
-  
-  
+    const { register, handleSubmit, errors } = useForm({
+        resolver: yupResolver(schema),
+    });
+    const newUser = (user) => {
+        console.log(user);
+    };
+
+
+
     return (
         <>
-         <Form onSubmit={handleSubmit(newUser)} >
-        
-         <Header  icon />
-        <Conteiner>
-          <Ola/> 
-        <Title1>Cadastro de Estufas </Title1>
-        
-        <InputText noIcon idFor="user" 
-            placeholder="Digite o nome da Estufa"
-             name="nomeestufa"
-             type="text"
-             labelText="Nome da Estufa:"
-             register={register} />
-        {errors.nomeestufa?.message  &&   <Modal 
-                    style={{marginTop:"25px"}}
-                    visible={false}
-                    titulo="Error"
-                    conteudo={errors.nomeestufa?.message}                   
-                    page="/login-estufas"                 
-                    ></Modal> }
-        <Button style={{marginTop:"25px"}} >Cadastrar</Button> 
-        
-        </Conteiner>
-        </Form>
+            <Form onSubmit={handleSubmit(newUser)} >
+
+                <Header icon />
+                <Conteiner>
+                    <Ola />
+                    <Title1>Cadastro de Estufas </Title1>
+
+                    <InputText noIcon idFor="user"
+                        placeholder="Digite o nome da Estufa"
+                        name="nomeestufa"
+                        type="text"
+                        labelText="Nome da Estufa:"
+                        register={register} />
+                    {errors.nomeestufa?.message && <Modal
+                        style={{ marginTop: "25px" }}
+                        visible={false}
+                        titulo="Error"
+                        conteudo={errors.nomeestufa?.message}
+                        page="/login-estufas"
+                    ></Modal>}
+                    <Button style={{ marginTop: "25px" }} >Cadastrar</Button>
+
+                </Conteiner>
+            </Form>
         </>
-        
+
     )
-    
+
 }
 export default CadastroEstufa;
