19ba36fd7e38cd7fafe1d7e7d59b2f950feab685
marlloncampos <marllondcsp@gmail.com>
2021-05-05 01:33:22 -0300

    Criado um componente para a mensagem de erro que vem do yup

---
+++ front-end/src/components/ErrorMessage/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const ErrorMessage = styled.p `
+  text-align:center;
+  font-size:14px;
+  color:var(--error-message);
+  font-family:Roboto, sans-serif;
+  margin:12px;
+`;
