9bc926d9f3c0f39878800bfe15f8c34016f75108
marlloncampos <marllondcsp@gmail.com>
2021-05-05 01:37:37 -0300

    Criado novos schemas para o login e criação de usuário

--- front-end/src/pages/LoginEstufa/validation.js
+++ front-end/src/pages/LoginEstufa/validation.js
@@ -5,4 +5,23 @@ const schema = yup.object().shape({
     email: yup.string().min(6,'Senha errada'),
 
 });
+
+
+export const createUserSchema = yup.object().shape({
+    name:yup.string().min(1,'O nome deve conter no minimo um caractere'),
+
+    email:yup.string().email('Insira um e-mail válido').required('O email é obrigatório'),
+
+    password:yup.string().min(5,'A senha deve conter no minimo 5 caracteres').required(),
+
+    confirm:yup.string().oneOf([yup.ref('password'), null], 'As senhas devem ser iguais').required('O campo de confirmação é obrigatório'),
+
+})
+
+
+export const loginSchema = yup.object().shape({
+    email: yup.string().email('Insira um e-mail válido').required('O email é obrigatório'),
+
+    password:yup.string().required('A senha é obrigatória').min(5, 'A senha deve conter no minimo 5 caracteres')
+})
 export default schema;
