c8a913598bb0c5a0bcd0296fbea29651a9b482ac
marlloncampos <marllondcsp@gmail.com>
2021-05-05 01:39:00 -0300

    Criado componente de loading, não sei onde usar ainda

---
+++ front-end/src/components/Loading/index.js
@@ -0,0 +1,15 @@
+
+import loadingIcon from '../../assets/loading.svg'
+import { Container } from './styles'
+
+const Loading = () => {
+    return (
+        <Container>
+            <img src={loadingIcon} width='100px'/>
+
+        </Container>
+    )
+
+}
+
+export default Loading
