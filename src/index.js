bacd45220ba2abcdd3aac6dce7d4a48d735a9973
JPFregni <jpfregni@gmail.com>
2021-08-23 21:57:41 -0300

    Organizando arquivos
    Organizando os arquivos para melhor entendimento

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Routes from './Routes'
 
+import './global.css'
+import Base from './styles/generic/elements/Base'
 import GlobalStyles from './GlobalStyles'
 import { UserInfoProvider } from './contexts/UserInfoContext'
 import Store from './contexts/Store'
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
+    <Base />
     <UserInfoProvider>
       <Routes />
     </UserInfoProvider>
