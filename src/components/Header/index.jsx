abb7c6757e65c3553c8c204a2ae49cd297b4348c
Marllon Campos <marlloncampos@hotmail.com>
2021-08-25 11:42:47 -0300

    feat: retirado o componente de sidemenu do header e do projeto

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,16 +8,11 @@ import {
   Input,
   CloseIcon,
   InputContainer,
-  BurgerIconContainer,
-  BurgerIcon,
-  Sidemenu,
-  SidemenuContainer,
 } from "./styles";
 
 function Header({ onChange,icon, ...props }) {
   const history = useHistory();
   const [isInputShowing, setIsInputShowing] = useState(false);
-  const [isSidemenuShowing, setIsSidemenuShowing] = useState(false);
   const [hasToWait, setHasToWait] = useState(false);
 
   function openInput() {
@@ -48,19 +43,6 @@ function Header({ onChange,icon, ...props }) {
         ) : (
           <ArrowIosBack onClick={history.goBack}size={38}/>
         )}
-      <BurgerIconContainer  onClick={() => setIsSidemenuShowing(!isSidemenuShowing)} >
-        <BurgerIcon
-          openAnimation={isSidemenuShowing}
-         
-        />
-      </BurgerIconContainer>
-        <Sidemenu openAnimation={isSidemenuShowing}>
-          <div style={{padding:'3%', backgroundColor:'#0B8722',display:'flex', alignItems:'center',  paddingRight:'45px' }}>
-            <img style={{borderRadius:'50%',}}src="https://via.placeholder.com/45/45" />
-            <p style={{marginLeft:'10px'}}>Marllon Campos</p>
-          </div>
-
-        </Sidemenu>
       </ContentWrapper>
     </Container>
   );
