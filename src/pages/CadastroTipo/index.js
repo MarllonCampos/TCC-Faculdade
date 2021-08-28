1d7d729e72b41e6533d0844ac893a352b7cbd11c
JPFregni <jpfregni@gmail.com>
2021-08-27 23:44:16 -0300

    Att pag Cadastro tipo
    - Tornando cadastro estufa em ambiguo, para ser utilizado tbm como cadastro planta.
    - Criando caminho de enviar dados de cada tipo, sem que haja conflito.

--- src/pages/CadastroTipo/index.js
+++ src/pages/CadastroTipo/index.js
@@ -1,62 +1,92 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { Conteiner, Title1, } from "./styles"
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { Conteiner, Title1 } from './styles'
 
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from "yup";
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 
 import Header from '../../components/Header'
-import Form from '../../components/Form';
+import Form from '../../components/Form'
 import Ola from '../../components/Ola'
-import InputText from '../../components/Input';
-import Button from '../../components/Button';
-import Modal from '../../components/Modal';
-
-
+import InputText from '../../components/Input'
+import Button from '../../components/Button'
+import Modal from '../../components/Modal'
+import { useParams } from 'react-router-dom'
 
 function CadastroTipo() {
+  const { tipo } = useParams()
 
-    const schema = yup.object().shape({
-        nomeestufa: yup.string().min(2, "Digite um nome para Estufa").required(),
-    });
-    const { register, handleSubmit, errors } = useForm({
-        resolver: yupResolver(schema),
-    });
-    const newUser = (user) => {
-        console.log(user);
-    };
+  let schema
 
+  if (tipo == 'estufa') {
+    schema = yup.object().shape({
+      nomeEstufa: yup.string().min(2, 'Digite um nome para Estufa').required()
+    })
+  } else {
+    schema = yup.object().shape({
+      nomePlanta: yup.string().min(2, 'Digite um nome para Planta').required()
+    })
+  }
 
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema)
+  })
+  async function newUser(tipoCadastro, ...rest) {
+    if (tipo == 'estufa') {
+      console.log('Criar Estufa', tipoCadastro.nomeEstufa)
+    } else {
+      console.log('Criar Planta', tipoCadastro.nomePlanta)
+    }
+  }
 
-    return (
-        <>
-            <Form onSubmit={handleSubmit(newUser)} >
+  return (
+    <>
+      <Header icon />
 
-                <Header icon />
-                <Conteiner>
-                    <Ola />
-                    <Title1>Cadastro de Estufas </Title1>
-
-                    <InputText noIcon idFor="user"
-                        placeholder="Digite o nome da Estufa"
-                        name="nomeestufa"
-                        type="text"
-                        labelText="Nome da Estufa:"
-                        register={register} />
-                    {errors.nomeestufa?.message && <Modal
-                        style={{ marginTop: "25px" }}
-                        visible={false}
-                        titulo="Error"
-                        conteudo={errors.nomeestufa?.message}
-                        page="/login-estufas"
-                    ></Modal>}
-                    <Button style={{ marginTop: "25px" }} >Cadastrar</Button>
-
-                </Conteiner>
-            </Form>
-        </>
-
-    )
-
+      <Conteiner>
+        <Ola style={{ marginTop: '-60px' }} />
+        {tipo == 'estufa' ? (
+          <Title1>Cadastro de Estufas </Title1>
+        ) : (
+          <Title1>Cadastro de Plantas </Title1>
+        )}
+        <Form onSubmit={handleSubmit(newUser)}>
+          {tipo == 'estufa' ? (
+            <InputText
+              noIcon
+              idFor="estufa"
+              placeholder="Digite o nome da Estufa"
+              name="nomeEstufa"
+              type="text"
+              labelText="Nome da Estufa:"
+              register={register}
+            />
+          ) : (
+            <InputText
+              noIcon
+              idFor="planta"
+              placeholder="Digite o nome da Planta"
+              name="nomePlanta"
+              type="text"
+              labelText="Nome da Planta:"
+              register={register}
+            />
+          )}
+          {errors?.nomeEstufa?.message && (
+            <Modal
+              style={{ marginTop: '25px' }}
+              visible={false}
+              titulo="Error"
+              conteudo={errors?.nomeEstufa?.message}
+              page="/login-estufas"
+            ></Modal>
+          )}
+          <Button type="submit" style={{ marginTop: '25px' }}>
+            Cadastrar
+          </Button>
+        </Form>
+      </Conteiner>
+    </>
+  )
 }
-export default CadastroTipo;
+export default CadastroTipo
--- src/pages/CadastroTipo/styles.js
+++ src/pages/CadastroTipo/styles.js
@@ -5,6 +5,7 @@ export const Conteiner = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   min-height: calc(100vh - 55px);
 `
 
