329a1fbfc5b5d3c3dc295a3413eccbe2b9e8fd1c
Rafael Bispo <Rafael_bispo23@hotmail.com>
2021-08-27 23:36:15 -0300

    criando feature de cadastro de usuario

---
+++ src/pages/CadastroUsuario/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+
+export const Label = styled.label`
+    color:var(--text);
+    font-weight:400;
+    padding-left:10px;
+    font-family:Poppins,sans-serif;
+`;
+export const A = styled.a`
+
+ 
+ color: white;
+ font-size: 20px;
+`;
+
+export const Span = styled.span`
+color: #FFF;
+margin-left:10px;
+  
+`;
