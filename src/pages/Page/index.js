6b301709b7caf20d1fa6a76f60eb7fdfbe3cbb38
Marllon Campos <marlloncampos@hotmail.com>
2021-08-28 12:04:03 -0300

    Revert "Merge pull request #5 from MarllonCampos/feature/criar-mock-de-dados"
    This reverts commit 2a4c2e63efbf87ddf9b22197389fdaa23c9adb37, reversing
    changes made to 8742a89148a681e31ad00900fdbd46d45693870a.

--- src/pages/Page/index.js
+++ src/pages/Page/index.js
@@ -1,171 +1,174 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
 
-
 import Header from '../../components/Header'
-import InputText from '../../components/Input';
-import OlaLogo from '../../components/Ola';
+import InputText from '../../components/Input'
+import OlaLogo from '../../components/Ola'
 import Select from '../../components/Select'
 import Button from '../../components/Button'
 import Modal from '../../components/Modal'
-import {api} from '../../utils/api'
-import { uploadImage } from '../../utils/firebase'
+import { api } from '../../utils/api'
 
 import { saveData, retrieveSessionData } from '../../utils/sessionStorage'
 import { UserInfoContext } from '../../contexts/UserInfoContext'
 
-
-
 import { Conteiner } from '../EstufaAtiva/styles'
 
-
-import { TituloCima, TituloBaixo, Wrapper, GridContainer, GridItem } from './styles'
+import {
+  TituloCima,
+  TituloBaixo,
+  Wrapper,
+  GridContainer,
+  GridItem
+} from './styles'
 
 function Page({ title, color, border, ...props }) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [image, setImage] = useState('');
-    const [url, setUrl] = useState('');
-    const [numero, setNumero] = useState(null)
-    const [isModalShowing, setIsModalShowing] = useState(false)
-    const [modalMessage, setModalMessage] = useState('')
-    const [modalTitle, setModalTitle] = useState('')
-    const [modalContent, setModalContent] = useState('')
-    const [selectState, setSelectState] = useState(false)
-    const { userName, setUserName, greenerys, setGreenerys, imagem, setImagem } = useContext(UserInfoContext)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [image, setImage] = useState('')
+  const [url, setUrl] = useState('')
+  const [numero, setNumero] = useState(null)
+  const [isModalShowing, setIsModalShowing] = useState(false)
+  const [modalMessage, setModalMessage] = useState('')
+  const [modalTitle, setModalTitle] = useState('')
+  const [modalContent, setModalContent] = useState('')
+  const [selectState, setSelectState] = useState(false)
+  const { userName, setUserName, greenerys, setGreenerys, imagem, setImagem } =
+    useContext(UserInfoContext)
 
+  useEffect(() => {
+    setNumero([
+      { name: 'Norte', status: true },
+      { name: 'Sul', status: false },
+      { name: 'Leste', status: true },
+      { name: 'Oeste', status: false }
+    ])
+    console.log(imagem)
+  }, [])
 
+  useEffect(() => {
+    console.log(url)
+  }, [url])
 
+  const handleClickLogin = async () => {
+    if (email == null || password == null)
+      return console.log('Precisa de password')
+    console.log('Tentando Realizar o Login')
+    setIsModalShowing(true)
+    setModalTitle('Tentando Realizar o Login')
+    setModalMessage('Aguarde alguns instantes e seu login sera realizado')
+    const response = await api.post('/login', {
+      email,
+      password
+    })
+    const data = await response.data
 
-    useEffect(() => {
-        setNumero([{ name: "Norte", status: true }, { name: "Sul", status: false }, { name: "Leste", status: true }, { name: "Oeste", status: false }])
-        console.log(imagem)
-    }, [])
+    if (data.message) {
+      setModalTitle('Falha em realizar o login')
+      setModalMessage(data.message.title)
+      setModalContent(data.message.content)
+      return
+    }
+    setModalTitle('Login realizado com sucesso')
+    setModalMessage(`Ola ${data.user}`)
+    setModalContent(`Seja bem-vindo`)
+    console.log(`Ola ${data.user} logado com sucesso`)
 
-    useEffect(() => {
-        console.log(url)
-    }, [url])
-
-
-
-
-    const handleClickLogin = async () => {
-        if (email == null || password == null) return console.log('Precisa de password')
-        console.log('Tentando Realizar o Login')
-        setIsModalShowing(true)
-        setModalTitle('Tentando Realizar o Login')
-        setModalMessage('Aguarde alguns instantes e seu login sera realizado')
-        const response = await api.post('/login', {
-            email,
-            password
-        })
-        const data = await response.data;
-
-
-
-        if (data.message) {
-            setModalTitle('Falha em realizar o login')
-            setModalMessage(data.message.title)
-            setModalContent(data.message.content)
-            return
-        }
-        setModalTitle('Login realizado com sucesso')
-        setModalMessage(`Ola ${data.user}`)
-        setModalContent(`Seja bem-vindo`)
-        console.log(`Ola ${data.user} logado com sucesso`)
-
-        if (saveData(data) == 'sucesso') {
-            console.log(retrieveSessionData('greeneryData'))
-
-        } else {
-            console.log('Falhou em salvar')
-        }
-
-
-        setTimeout(() => {
-            setIsModalShowing(false)
-        }, 2000)
-
-
-        setGreenerys(data.greenerys)
-        setUserName(data.user)
-
-
+    if (saveData(data) == 'sucesso') {
+      console.log(retrieveSessionData('greeneryData'))
+    } else {
+      console.log('Falhou em salvar')
     }
 
-    function mostrar(e) {
-        console.log(e.target.value)
+    setTimeout(() => {
+      setIsModalShowing(false)
+    }, 2000)
+
+    setGreenerys(data.greenerys)
+    setUserName(data.user)
+  }
+
+  function mostrar(e) {
+    console.log(e.target.value)
+  }
+
+  const handleChange = e => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0])
     }
+  }
 
-    const handleChange = (e) => {
-        if (e.target.files[0]) {
-            setImage(e.target.files[0])
-        }
-    }
+  function clearModalMessage() {
+    setModalContent('')
+    setModalMessage('')
+    setModalTitle('')
+  }
 
-    const upload = async () => {
-        const urlName = await uploadImage(image,setUrl);
-    }
+  return (
+    <Conteiner>
+      <Header onChange={mostrar} icon />
+      <Wrapper>
+        {isModalShowing && (
+          <Modal
+            onClose={() => {
+              setIsModalShowing(false)
+              clearModalMessage()
+            }}
+            titulo={modalTitle}
+            conteudo={modalMessage || ''}
+            conteudo1={modalContent || ''}
+          />
+        )}
+        <OlaLogo />
+        <h5 style={{ textAlign: 'center' }}> Resgate sua conta! </h5>
+        {userName && (
+          <GridContainer>
+            <GridItem to="cadastra-elementos">Cadastra Elementos</GridItem>
+            <GridItem to="cadastro-estufas">Cadastro Estufa</GridItem>
+            <GridItem to="estufa-ativa">Estufa Ativa</GridItem>
+            <GridItem to="list-estufas">List Estufas</GridItem>
+            <GridItem to="login-estufas">Login Estufa</GridItem>
+            <GridItem to="cadastro">Cadastro</GridItem>
+            <GridItem to="uploader-login">Login Estufa</GridItem>
+            <GridItem to="recuperar">Login Estufa</GridItem>
+            {numero && (
+              <Link
+                className="last"
+                to={{
+                  pathname: '/description-card',
+                  elements: numero,
+                  icon: 'fan'
+                }}
+              >
+                Description Card{' '}
+              </Link>
+            )}
+          </GridContainer>
+        )}
 
-    function clearModalMessage() {
-        setModalContent('')
-        setModalMessage('')
-        setModalTitle('')
-    }
+        <InputText
+          noIcon
+          onChange={e => setEmail(e.target.value)}
+          type="email"
+          labelText="E-mail:"
+          required
+        />
 
-    return (
-        <Conteiner>
+        <InputText
+          idFor="password"
+          onChange={e => setPassword(e.target.value)}
+          labelText="Senha:"
+        />
 
-            <Header onChange={mostrar} icon/>
-            <Wrapper>
-
-                {isModalShowing && <Modal onClose={() => { setIsModalShowing(false); clearModalMessage() }} titulo={modalTitle} conteudo={modalMessage || ''} conteudo1={modalContent || ''} />}
-                <OlaLogo />
-                <h5 style={{ textAlign: 'center' }}> Resgate sua conta! </h5>
-                {userName && (<GridContainer>
-                    <GridItem to="cadastra-elementos">Cadastra Elementos</GridItem>
-                    <GridItem to="cadastro-estufas">Cadastro Estufa</GridItem>
-                    <GridItem to="estufa-ativa">Estufa Ativa</GridItem>
-                    <GridItem to="list-estufas">List Estufas</GridItem>
-                    <GridItem to="login-estufas">Login Estufa</GridItem>
-                    <GridItem to="cadastro">Cadastro</GridItem>
-                    <GridItem to="uploader-login">Login Estufa</GridItem>
-                    <GridItem to="recuperar">Login Estufa</GridItem>
-                    {numero && <Link
-                        className="last"
-                        to={{
-                            pathname: "/description-card",
-                            elements: numero,
-                            icon: "fan"
-                        }}
-                    >Description Card </Link>}
-                </GridContainer>)}
-
-                <InputText
-                    noIcon
-                    onChange={(e) => setEmail(e.target.value)}
-                    type="email"
-                    labelText="E-mail:"
-                    required
-                />
-
-                <InputText
-                    idFor="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    labelText="Senha:"
-                />
-
-                <input type="file" onChange={handleChange} />
-                {url}
-
-                <Button disabled={!image || image == ''} onClick={upload }>Upload</Button>
-
-                <Button disabled={email.length === 0 || password.length === 0} onClick={handleClickLogin}>Cadastrar</Button>
-
-
-            </Wrapper>
-        </Conteiner>
-    )
+        <Button
+          disabled={email.length === 0 || password.length === 0}
+          onClick={handleClickLogin}
+        >
+          Cadastrar
+        </Button>
+      </Wrapper>
+    </Conteiner>
+  )
 }
 
-export default Page;
+export default Page
