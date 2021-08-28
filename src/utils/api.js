fa679d72847533942a32dcc3173b9147b07b2fcc
Marllon Campos <marlloncampos@hotmail.com>
2021-08-28 12:04:03 -0300

    Revert "Merge pull request #5 from MarllonCampos/feature/criar-mock-de-dados"
    This reverts commit 2a4c2e63efbf87ddf9b22197389fdaa23c9adb37, reversing
    changes made to 8742a89148a681e31ad00900fdbd46d45693870a.

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,10 @@
 import axios from 'axios';
 
-const config = {
-    baseURL: process.env.REACT_APP_LOCAL === 'dev' ? 'http://localhost:3003' : process.env.REACT_APP_API_URL,
-    timeout: 20000,
 
-}
-
-
-const api = axios.create(config);
+const api = axios.create({
+    baseURL: "https://backend-greenery.herokuapp.com",
+    timeout:20000,
+});
 
 
 
