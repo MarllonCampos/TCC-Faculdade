75d5e05f92b26336b34b111cf0988a568ffbf57f
JPFregni <jpfregni@gmail.com>
2021-08-29 19:22:22 -0300

    Update index.js
    - add funções
    - fazendo que a pagina traga info do back

--- src/pages/ListaPlanta/index.js
+++ src/pages/ListaPlanta/index.js
@@ -1,22 +1,40 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Card from '../../components/Card'
 import Header from '../../components/Header'
+import Title from '../../components/Title'
 import { Conteiner } from './styles'
 import { UserInfoContext } from '../../contexts/UserInfoContext'
+import { api } from '../../utils/api'
 
 function ListaPlanta() {
-  const { userName, greenerys } = useContext(UserInfoContext)
-
+  const [plantas, setPlantas] = useState([])
   useEffect(() => {
-    console.log(userName, greenerys)
+    async function fetchData() {
+      const resposta = await api.get('/plantas', {
+        headers: { 'User-Email': 'marllon' }
+      })
+
+      const data = await resposta.data
+      setPlantas(data)
+      console.log(plantas, data)
+    }
+    fetchData()
   }, [])
 
   return (
     <>
       <Header />
-      <Conteiner>
-        <Card title="Batata Frita" date="2021/08/21" />
+      <Conteiner style={{ backgroundColor: 'var(--quartiary)' }}>
+        <Title title="teste" />
+        {plantas.map(planta => (
+          <Card
+            title={planta.nomeplanta}
+            date={planta.dataplanta}
+            imagem={planta.imagem}
+          />
+        ))}
+
         <Link to="cadastro-tipo/planta">+</Link>
       </Conteiner>
     </>
