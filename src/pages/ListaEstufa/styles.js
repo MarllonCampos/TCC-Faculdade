6268e3676ee9da723371425607d11f1c05f44f6c
JPFregni <jpfregni@gmail.com>
2021-09-02 22:18:35 -0300

    estilização da pagina lista estufa
    colocando um padding-bottom de 15 pixels para  dar uma margem no final da pagina

--- src/pages/ListaEstufa/styles.js
+++ src/pages/ListaEstufa/styles.js
@@ -1,9 +1,9 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const Conteiner = styled.div`
-    display:flex;
-    flex-direction:column;
-    align-items:center;
-    min-height:calc(100vh - 55px);
-    
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: calc(100vh - 55px);
+  padding-bottom: 15px;
 `
