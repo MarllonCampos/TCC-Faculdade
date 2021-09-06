9fdf2e36750f5326afadbb29a18c5badf9726152
Marllon Campos <marlloncampos@hotmail.com>
2021-09-06 13:44:32 -0300

    feat: criado uma lógica para escolher entre o mock de dados no computador  a API em produção

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,13 @@
-import axios from 'axios';
+import axios from 'axios'
 
+const config = {
+  baseURL:
+    process.env.REACT_APP_LOCAL === 'dev'
+      ? 'http://localhost:3003'
+      : process.env.REACT_APP_API_URL,
+  timeout: 20000
+}
 
-const api = axios.create({
-    baseURL: "https://backend-greenery.herokuapp.com",
-    timeout:20000,
-});
+const api = axios.create(config)
 
-
-
-export  {api};
+export { api }
