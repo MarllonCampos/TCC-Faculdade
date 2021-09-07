19609c4611dd97b0ba168a8c8604515785e75abb
JPFregni <jpfregni@gmail.com>
2021-09-06 22:46:11 -0300

    Cadastro estufa - Teste SweetAlert
    - Adicionado na tela Cadastro Estufa, um modal de teste do SweetAlert.
    - O modal funciona, aparecendo o mesmo, o único problema é que ao entrar na página o modal já ativa uma vez e também aparece um erro no console dizendo:
    
    Warning: Expected `onClick` listener to be a function, instead got a value of `object` type

--- src/pages/CadastroEstufa/index.js
+++ src/pages/CadastroEstufa/index.js
@@ -1,62 +1,62 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { Conteiner, Title1, } from "./styles"
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { Conteiner, Title1 } from './styles'
 
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from "yup";
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 
 import Header from '../../components/Header'
-import Form from '../../components/Form';
+import Form from '../../components/Form'
 import Ola from '../../components/Ola'
-import InputText from '../../components/Input';
-import Button from '../../components/Button';
-import Modal from '../../components/Modal';
+import InputText from '../../components/Input'
+import Button from '../../components/Button'
+import Modal from '../../components/Modal'
 
-
+import ReactSwal from 'sweetalert2'
 
 function CadastroEstufa() {
+  const schema = yup.object().shape({
+    nomeestufa: yup.string().min(2, 'Digite um nome para Estufa').required()
+  })
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema)
+  })
+  const newUser = user => {
+    console.log(user)
+  }
 
-    const schema = yup.object().shape({
-        nomeestufa: yup.string().min(2, "Digite um nome para Estufa").required(),
-    });
-    const { register, handleSubmit, errors } = useForm({
-        resolver: yupResolver(schema),
-    });
-    const newUser = (user) => {
-        console.log(user);
-    };
+  async function testeCliqueBotao() {
+    ReactSwal.fire({
+      icon: 'success',
+      title: 'Sucessoo',
+      text: 'Estufa cadastrada com sucesso'
+    })
+  }
 
+  return (
+    <>
+      <Form onSubmit={handleSubmit(newUser)}>
+        <Header icon />
+        <Conteiner>
+          <Ola />
+          <Title1>Cadastro de Estufas </Title1>
 
+          <InputText
+            noIcon
+            idFor="user"
+            placeholder="Digite o nome da Estufa"
+            name="nomeestufa"
+            type="text"
+            labelText="Nome da Estufa:"
+            register={register}
+          />
 
-    return (
-        <>
-            <Form onSubmit={handleSubmit(newUser)} >
-
-                <Header icon />
-                <Conteiner>
-                    <Ola />
-                    <Title1>Cadastro de Estufas </Title1>
-
-                    <InputText noIcon idFor="user"
-                        placeholder="Digite o nome da Estufa"
-                        name="nomeestufa"
-                        type="text"
-                        labelText="Nome da Estufa:"
-                        register={register} />
-                    {errors.nomeestufa?.message && <Modal
-                        style={{ marginTop: "25px" }}
-                        visible={false}
-                        titulo="Error"
-                        conteudo={errors.nomeestufa?.message}
-                        page="/login-estufas"
-                    ></Modal>}
-                    <Button style={{ marginTop: "25px" }} >Cadastrar</Button>
-
-                </Conteiner>
-            </Form>
-        </>
-
-    )
-
+          <Button onClick={testeCliqueBotao()} style={{ marginTop: '25px' }}>
+            Cadastrar
+          </Button>
+        </Conteiner>
+      </Form>
+    </>
+  )
 }
-export default CadastroEstufa;
+export default CadastroEstufa
