161948d813e60211f0d2946af05cc25c6a62be73
JPFregni <jpfregni@gmail.com>
2021-09-09 22:38:30 -0300

    Ajustes no loading
    Ajustado a propriedade do icone loading, para aparece como verde

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,15 +1,17 @@
-
 import loadingIcon from '../../assets/loading.svg'
+import loadingIconVerde from '../../assets/loading-verde.svg'
 import { Container } from './styles'
 
-const Loading = () => {
-    return (
-        <Container>
-            <img src={loadingIcon} width='100px'/>
-
-        </Container>
-    )
-
+const Loading = props => {
+  return (
+    <Container>
+      {props.verde ? (
+        <img src={loadingIconVerde} width="100px" />
+      ) : (
+        <img src={loadingIcon} width="100px" />
+      )}
+    </Container>
+  )
 }
 
 export default Loading
