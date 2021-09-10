81cf8330b53ff60adcbb9b941fff2cc89f015f75
Marllon Campos <marlloncampos@hotmail.com>
2021-09-10 15:52:59 -0300

    feat: adicionado um modal para informar o usuário quando acessar a rota lista-elemento sem logar

--- src/pages/ListaElemento/index.js
+++ src/pages/ListaElemento/index.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react'
-import { useParams } from "react-router-dom";
 import Header from '../../components/Header'
 import { BotaoElemento, Container } from './styles'
 import IrrigadorElemento from '../../assets/irrigadorElemento.svg'
@@ -9,21 +8,61 @@ import ArrowDown from '../../assets/arrow-down-branco.svg'
 import { RenderizaListaElemento } from '../../components/RenderizaListaElemento'
 import {api} from '../../utils/api'
 import Loading from '../../components/Loading'
+import {ReactSwal} from '../../components/ReactSwal'
+import {useHistory} from 'react-router-dom'
 export function ListaElemento(){
+  const history = useHistory();
     const [setaIrrigadoresAparece,setSetaIrrigadoresAparece] = useState(false)
   const [setaLuzesAparece,setSetaLuzesAparece] = useState(false)
   const [setaVentiladoresAparece,setSetaVentiladoresAparece] = useState(false)
   const [estufa,setEstufa] = useState()
-  const {idEstufa} = useParams();
+
   function reverteEstado(setEstado){
     setEstado( prevState => !prevState);
   }
 
   useEffect(()=>{
     async function fetchData(){
-      const response = await api.get(`/estufas/${idEstufa}`)
-      const estufas = await response.data
-      setEstufa(estufas)
+      let userData
+      try{
+        userData = JSON.parse(localStorage.getItem('userInfo'))
+
+        if (!userData){
+          // eslint-disable-next-line no-throw-literal
+          throw "Você não esta logado"
+        }
+      }
+      catch(error){
+        return ReactSwal.fire({
+          title:error,
+          text: 'Sera enviado para a tela de login',
+          timer:3000,
+          timerProgressBar:true,
+          showCancelButton:false,
+          showConfirmButton:false,
+          willClose: () => history.push('/')
+        })
+      }
+      try{
+        const response = await api.get(`/greenery `,{
+          headers:{
+            'user-email':userData.email
+          }
+        })
+        const estufas = await response.data
+        setEstufa(estufas)
+      }catch(error) {
+        console.log(error)
+        return ReactSwal.fire({
+          title:'Ops',
+          text: 'Houve um erro no servidor tente novamente mais tarde, agradecemos pela paciência',
+          timer:3500,
+          timerProgressBar:true,
+          showCancelButton:false,
+          showConfirmButton:false,
+          willClose: () => history.goBack()
+        })
+      }
       
     }
     if (!estufa) {
