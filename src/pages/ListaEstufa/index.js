23e431499a0b65c500e7aef692d6970dcc103c4b
JPFregni <jpfregni@gmail.com>
2021-09-09 22:42:36 -0300

    Ajustes Lista estufa
    importado loading, router, useHistory, api, local data, criado uma const do history, criado um estado para a estufa.
    Criado uma forma de receber o email do usuario,
    colocado uma condição para aparecer a estufa ou o loading

--- src/pages/ListaEstufa/index.js
+++ src/pages/ListaEstufa/index.js
@@ -2,13 +2,14 @@ import React, { useContext, useEffect, useState, useRef } from 'react'
 import Card from '../../components/card'
 import Header from '../../components/Header'
 import { Conteiner } from './styles'
-import BotaoMais from '../../components/BotaoMais'
-import { UserInfoContext } from '../../contexts/UserInfoContext'
+import Loading from '../../components/Loading'
+import { Router, useHistory } from 'react-router'
+import { api } from '../../utils/api'
+import { localData } from '../../utils/localStorage'
 
 function ListaEstufa() {
-  const { userName, greenerys } = useContext(UserInfoContext)
-
-  const [filtrarEstufa, setFiltrarEstufa] = useState(false)
+  const history = useHistory()
+  const [estufa, setEstufa] = useState(false)
   const timeoutRef = useRef(null)
 
   function SetDebounce(event, fn, delay) {
@@ -21,41 +22,57 @@ function ListaEstufa() {
   }
 
   useEffect(() => {
-    console.log(userName, greenerys)
+    const { email, user } = localData('userInfo')
+
+    async function fetchData() {
+      const resposta = await api.get('/greenery', {
+        headers: { 'user-email': email }
+      })
+
+      const data = await resposta.data
+      setEstufa(data)
+    }
+    fetchData()
   }, [])
 
   return (
     <>
       <Header />
-      <Conteiner>
-        {greenerys.map((estufa, index) => (
-          <Card
-            cliqueLongo={() => console.log('longo')}
-            cliqueCurto={() => console.log('curto')}
-            key={`${estufa.nomestufa}-${index}`}
-            title={estufa.nomeestufa}
-            irrigacao={
-              estufa.elementos.filter(
-                elemento => elemento.tipoelem == 'Água' && elemento.ativo != 0
-              ).length > 0
-            }
-            luz={
-              estufa.elementos.filter(
-                elemento => elemento.tipoelem == 'Luz' && elemento.ativo != 0
-              ).length > 0
-            }
-            ventilador={
-              estufa.elementos.filter(
-                elemento => elemento.tipoelem == 'Vento' && elemento.ativo != 0
-              ).length > 0
-            }
-            date={estufa.dataestufa || '00/00/00'}
-            imagem={estufa.fotoestufa}
-            titulo={estufa.nomeestufa}
-            elementos={estufa.elementos}
-          />
-        ))}
-      </Conteiner>
+      {estufa ? (
+        <Conteiner>
+          {estufa.map((estufa, index) => (
+            <Card
+              onClick={() => {
+                history.push(`/estufa-ativa/${estufa.idestufa}`)
+              }}
+              key={`${estufa.nomestufa}-${index}`}
+              title={estufa.nomeestufa}
+              irrigacao={
+                estufa.elementos.filter(
+                  elemento => elemento.tipoelem == 'Água' && elemento.ativo != 0
+                ).length > 0
+              }
+              luz={
+                estufa.elementos.filter(
+                  elemento => elemento.tipoelem == 'Luz' && elemento.ativo != 0
+                ).length > 0
+              }
+              ventilador={
+                estufa.elementos.filter(
+                  elemento =>
+                    elemento.tipoelem == 'Vento' && elemento.ativo != 0
+                ).length > 0
+              }
+              date={estufa.dataestufa || '00/00/00'}
+              imagem={estufa.fotoestufa}
+              titulo={estufa.nomeestufa}
+              elementos={estufa.elementos}
+            />
+          ))}
+        </Conteiner>
+      ) : (
+        <Loading verde />
+      )}
     </>
   )
 }
