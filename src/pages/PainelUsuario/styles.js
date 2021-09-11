0909e4d818ea9dc91ea1c0f9ca9fed93617e205f
Marllon Campos <marlloncampos@hotmail.com>
2021-09-10 23:57:36 -0300

    feat: criado a pagina PainelUsuario com seus componentes

---
+++ src/pages/PainelUsuario/styles.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  min-height:calc(100vh - 55px);
+  width:100%;
+  background:var(--quartiary);
+  padding:20px 0;
+  
+  
+  
+  form {
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    margin:0 auto;
+    padding:0 15px;
+    max-width: 450px;
+
+    & > a {
+      color:var(--text);
+      font-size:16px;
+      text-align:right;
+      font-weight:600;
+      text-decoration:underline;
+      font-family:Poppins, sans-serif;
+      align-self:flex-end;
+      
+    }
+    & > div {
+      margin-left:0;
+      margin-right:0;
+      align-self:flex-start;  
+
+    }
+    .buttons-container{
+      display:flex;
+      align-items:center;
+      justify-content:space-between;
+      gap: 15px;
+      width:100%;
+
+      button {
+        border-radius:var(--radius);
+      }
+
+      .cancelar {
+        background:#CA2A2A;
+      }
+    }
+  }
+
+`;
