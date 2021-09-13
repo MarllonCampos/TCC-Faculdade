2f66a1e5f2616e056aa483c994d2ee9d0d37c2cb
rafael_bispo23@hotmail.com <rafael_bispo23@hotmail.com>
2021-09-13 01:12:58 -0300

    Ajustes Login Usuario
    Fazendo ajuste no modal de erro e de sucesso e incluindo componente do Loading

--- src/pages/LoginUsuario/index.js
+++ src/pages/LoginUsuario/index.js
@@ -1,17 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Conteiner, A } from "./styles";
 import Title from "../../components/Title";
 import Button from "../../components/Button";
 import Modal from "../../components/Modal";
-import { saveData, retrieveSessionData } from '../../utils/sessionStorage'
+
+import { saveData, retrieveSessionData } from "../../utils/sessionStorage";
 import Form from "../../components/Form";
 import Main from "../../components/Main";
 import InputText from "../../components/Input";
 import Ola from "../../components/Ola";
 import { api } from "../../utils/api";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import Loading from "../../components/Loading";
 
+const MySwal = withReactContent(Swal);
 function LoginUsuario() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 400);
+    return () => clearTimeout(timer);
+  });
   const [usuario, setUsuario] = useState("");
   const [password, setPassword] = useState("");
   const [isModalShowing, setIsModalShowing] = useState(false);
@@ -25,72 +38,81 @@ function LoginUsuario() {
   }
 
   const handleClickLogin = async () => {
-    if (usuario == null || password == null)
-      return console.log("Precisa de password");
-    console.log("Tentando Realizar o Login");
-    setIsModalShowing(true);
-    setModalTitle("Testando Realizar o Login");
-    setModalMessage("Aguarde alguns instantes e seu login sera realiado");
+    setLoading(true);
+
     const response = await api.post("/login", {
       usuario,
       password,
     });
     const data = await response.data;
+    console.log(data);
 
     if (data.message) {
-      setModalTitle("Fala em realizar o login");
-      setModalMessage(data.message.title);
-      setModalContent(data.message.content);
+      MySwal.fire({
+        icon: "error",
+        title: `${data.message.title}`,
+        text: `${data.message.content}`,
+      });
       return;
     }
-    setModalTitle("Login realizado com sucesso");
-    setModalMessage(`Ola ${data.user}`);
-    setModalContent(`Seja bem-vindo`);
+    
+    MySwal.fire({
+      icon: "success",
+      title: "Login realizado com sucesso",
+      text: `Ola ${data.user}`,
+      text: "Seja bem-vindo",
+      
+    });
     console.log(`Ola ${data.user} logado com sucesso`);
+    
+    
 
-    if (saveData(data) == 'sucesso') {
-        console.log(retrieveSessionData('greeneryData'))
-      } else {
-        console.log('Falhou em salvar')
-      }
+    if (saveData(data) == "sucesso") {
+      console.log(retrieveSessionData("greeneryData"));
+    } else {
+      console.log("Falhou em salvar");
+    }
   };
 
   return (
     <Main>
-      <Form
-        onSubmit={(event) => {
-          event.preventDefault();
-          console.log("Usuario", usuario, "senha", password);
-        }}
-      >
-        <Ola></Ola>
-        <Title title="Entre com sua conta" />
-
-        <InputText
-          noIcon
-          onChange={(e) => setUsuario(e.target.value)}
-          type="text"
-          labelText="usuario:"
-          required
-        />
-
-        <InputText
-          idFor="password"
-          onChange={(e) => setPassword(e.target.value)}
-          labelText="Senha:"
-        />
-
-        <Button
-          disabled={usuario.length === 0 || password.length === 0}
-          onClick={handleClickLogin}
+      {loading && <Loading></Loading>}
+      {!loading && (
+        <Form
+          onSubmit={(event) => {
+            event.preventDefault();
+            console.log("Usuario", usuario, "senha", password);
+          }}
         >
-          Acessar
-        </Button>
-        <Conteiner>
-          <A href="/cadastro-usuario"> Me cadastra :) </A>
-          <A href="/recuperar">Recuperar</A>
-        </Conteiner>
-      </Form>
+          <Ola></Ola>
+          <Title title="Entre com sua conta" />
+
+          <InputText
+            noIcon
+            onChange={(e) => setUsuario(e.target.value)}
+            type="text"
+            labelText="usuario:"
+            required
+          />
+
+          <InputText
+            idFor="password"
+            onChange={(e) => setPassword(e.target.value)}
+            labelText="Senha:"
+          />
+
+          <Button
+            disabled={usuario.length === 0 || password.length === 0}
+            onClick={handleClickLogin}
+          >
+            Acessar
+          </Button>
+          <Conteiner>
+            <A href="/cadastro-usuario"> Me cadastra :) </A>
+            <A href="/recuperar">Recuperar</A>
+          </Conteiner>
+        </Form>
+      )}
     </Main>
   );
 }
