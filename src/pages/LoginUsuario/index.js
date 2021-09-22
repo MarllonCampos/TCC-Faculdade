4a6a8aade6c3397ae9b06c5872b12e0b1570b0c2
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2021-09-22 00:56:21 -0300

    feat: realizando o login e salvando no localStorage

--- src/pages/LoginUsuario/index.js
+++ src/pages/LoginUsuario/index.js
@@ -1,33 +1,119 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import {useHistory} from 'react-router-dom'
+import axios from "axios";
 
 import { ButtonContainer, A } from "./styles";
 import Title from "../../components/Title";
 import Button from "../../components/Button";
-import Modal from "../../components/Modal";
 
-import { saveData, retrieveSessionData } from "../../utils/sessionStorage";
 import Form from "../../components/Form";
 import Main from "../../components/Main";
 import InputText from "../../components/Input";
 import Ola from "../../components/Ola";
+import {ReactSwal} from '../../components/ReactSwal'
 import { api } from "../../utils/api";
-import Swal from "sweetalert2";
-import withReactContent from "sweetalert2-react-content";
-import Loading from "../../components/Loading";
+import {capitalizeFirstLetter} from '../../utils/capitalizeFirstLetter'
 
 export function LoginUsuario() {
+  const history = useHistory();
   const [loginState, setLoginState] = useState({
     email:'',
     senha:''
   });
 
-  function handleSubmitLogin(event) {
+
+  const Toast = ReactSwal.mixin({
+  // Cria o modelo de pop-up
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 2000,
+  timerProgressBar: true,
+  didOpen: toast => {
+    toast.addEventListener('mouseenter', ReactSwal.stopTimer)
+    toast.addEventListener('mouseleave', ReactSwal.resumeTimer)
+  }
+})
+
+  async function handleSubmitLogin(event) {
     event.preventDefault()
+    const {email,senha} = loginState
+    console.log(loginState)
+    const ifRequestCancelled = axios.CancelToken.source() // Isso aqui informa para o axios quando a req. foi cancelada
+
+    if (loginState.email === '' || loginState.email.includes('@') === false) {
+      return Toast.fire({
+        icon: 'error',
+        title: 'Digite um email válido',
+        timerProgressBar: true,
+        timer: 2500
+      })
+    } else if (loginState.senha.length < 7 ) {
+      return Toast.fire({
+        icon: 'error',
+        title: 'A senha deve conter no mínimo 7 caracteres',
+        timerProgressBar: true,
+        timer: 1000
+      })
+    }
+
+    ReactSwal.fire({
+      // Aparece a modal de loading para fazer o login
+      title: 'Realizando Login',
+      text: 'Aguarde alguns instantes e seu login será realizado',
+      didOpen: () => ReactSwal.showLoading(),
+      timer: 5000
+    }).then(result => {
+      if (result.dismiss === ReactSwal.DismissReason.timer) {
+        // Ao demorar mais de 5 segundos mostra modal deerro
+        ReactSwal.fire({
+          icon: 'info',
+          title: 'Aguarde uns instantes',
+          text: 'Esta demorando mais do que o esperado',
+          willClose: () => ifRequestCancelled.cancel()
+        })
+      }
+    })
+    const {data} = await api.post('/user/login/', {
+      email,
+      senha
+    }, {
+      cancelToken: ifRequestCancelled.token
+    })
+    if (data.status.toLowerCase() === 'ok') {
+      ReactSwal.hideLoading()
+      ReactSwal.fire({
+        title:capitalizeFirstLetter(data.mensagem.titulo),
+        text:"Login realizado com sucesso você sera redirecionado para as suas estufas",
+        icon:'success',
+        showConfirmButton:false,
+        timer:4500,
+        timerProgressBar:true,
+        willClose: () => {
+          const {nome,email,id} = data.mensagem.conteudo
+          localStorage.setItem('userInfo',JSON.stringify({user:nome,email,id}))
+          history.push('/lista-estufas')
+
+        }
+      })
+    } else if (data.status.toLowerCase() ==="erro") {
+      ReactSwal.fire({
+        title: capitalizeFirstLetter(data.mensagem.titulo),
+        text:data.mensagem.conteudo,
+        icon:'error',
+      })
+     
+    }
+
+    
+
+
   }
 
   function handleInputChange(event) {
     const {name,value} = event.target
     setLoginState(prevState => ({...prevState, [name]:value}))
+
   }
  
   return (
@@ -36,24 +122,27 @@ export function LoginUsuario() {
         <Form
           onSubmit={(event) => handleSubmitLogin(event)}
         >
-          <Ola></Ola>
-          <Title title="Entre com sua conta" />
+          <Ola style={{marginBottom:32}}/>
+          <Title style={{marginBottom:32}}title="Entre com sua conta" />
 
           <InputText
             noIcon
             onChange={(ev) => handleInputChange(ev)}
             type="text"
-            labelText="usuario:"
+            name="email"
+            labelText="Email:"
             required
+            onClick={() => localStorage.setItem('userInfo','')}
           />
 
           <InputText
             idFor="password"
+            name="senha"
             onChange={(ev) => handleInputChange(ev)}
             labelText="Senha:"
           />
 
-          <Button>
+          <Button style={{marginTop:42}}>
             Acessar
           </Button>
           <ButtonContainer>
