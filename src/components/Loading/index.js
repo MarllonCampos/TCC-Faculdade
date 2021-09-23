19c278fe6635fb364495c806f8b760cdadc3cd65
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2021-09-23 01:34:25 -0300

    feat: adicionado um novo tipo de loading que agora sera verde

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,11 +1,12 @@
 
 import loadingIcon from '../../assets/loading.svg'
+import loadingIconVerde from '../../assets/loading-verde.svg'
 import { Container } from './styles'
 
-const Loading = () => {
+const Loading = (props) => {
     return (
         <Container>
-            <img src={loadingIcon} width='100px'/>
+            <img src={`${props.verde ? loadingIconVerde : loadingIcon   }`}width='100px' alt="Carregando"/>
 
         </Container>
     )
