dcc9adc03e6d34b6ef747cc4c75d9c03c04dbbd0
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2021-10-03 19:13:04 -0300

    feat: arrumado o card para ficar de acordo com o design

--- src/components/CardDetalhe/index.jsx
+++ src/components/CardDetalhe/index.jsx
@@ -1,26 +1,23 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLightbulb, faFan, faWater } from '@fortawesome/free-solid-svg-icons'
+import {
+  Container,
+  LeftMessage,
+  Element,
+  Unit,
+} from "./styles";
 
-import {Container, RowTop, Element, ActiveElements, Unit} from './styles'
-
-const element = <FontAwesomeIcon icon={faFan} />
-
-
-const CardStatus = props => { 
-return (
-    <Container to={{pathname:"/description-card", elements:props.elementos, icon:props.tipo}} lastItem={props.lastItem} cor={props.cor}>
-        <RowTop>
-            <Element>{props.tipo}</Element><FontAwesomeIcon icon={props.iconess}/>
-        </RowTop>
-        <ActiveElements>{props.ativos} {props.planta_ou_ativa}</ActiveElements>
-        <Unit>
-            <p>Instalado </p>
-
-            {props.quant} uni
-        </Unit>
+const CardStatus = (props) => {
+  return (
+    <Container to={props.to} cor={props.cor}>
+      <LeftMessage>
+        <FontAwesomeIcon icon={props.iconess} size="lg" />
+        <Element>{props.tipo}</Element>
+      </LeftMessage>
+      <Unit>ver detalhes</Unit>
     </Container>
-)}
+  );
+};
 
-export default CardStatus
+export default CardStatus;
