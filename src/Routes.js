3071486ba7efc17c56fdee2bdef3d020da54adf2
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2021-10-03 17:18:40 -0300

    feat: removido rotas desnecessárias e colocado a rota padrão em primeira posição com o exact

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import Page from './pages/Page'
 
-import CadastroEstufas from './pages/CadastroTipo'
-import CadastroElemento from './pages/CadastroElemento'
 import EstufaAtiva from './pages/EstufaAtiva'
 import ListaEstufa from './pages/ListaEstufa'
 import ListaPlanta from './pages/ListaPlanta'
-import Modal from './components/Modal'
 import Cadastro from './pages/CadastroUsuario'
 import { ListaElemento } from './pages/ListaElemento'
 import { LoginUsuario } from './pages/LoginUsuario'
@@ -21,13 +17,9 @@ function Routes() {
         <Route path="/" exact component={LoginUsuario} />
         <Route path="/lista-estufas" component={ListaEstufa} />
         <Route path="/lista-plantas" component={ListaPlanta} />
-        <Route path="/modal" component={Modal} />
 
         <Route path="/estufa-ativa/:id" component={EstufaAtiva} />
         <Route path="/cadastro-usuario" component={Cadastro} />
-        <Route path="/cadastro-estufas" component={CadastroEstufas} />
-        <Route path="/cadastro-tipo/:tipo" component={CadastroEstufas} />
-        <Route path="/cadastro-elemento" component={CadastroElemento} />
         <Route path="/lista-elemento" component={ListaElemento} />
         <Route path="/painel-usuario" component={PainelUsuario} />
       </Switch>
