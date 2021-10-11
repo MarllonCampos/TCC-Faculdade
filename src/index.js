70def48482efdbb661dfe29451310ef1d56a9a0d
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2021-10-11 12:47:14 -0300

    feat: removido arquivos desnecessários

--- src/index.js
+++ src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom'
 import Routes from './Routes'
 
 import GlobalStyles from './GlobalStyles'
-import { UserInfoProvider } from './contexts/UserInfoContext'
-import Store from './contexts/Store'
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
-    <UserInfoProvider>
-      <Routes />
-    </UserInfoProvider>
+    <Routes />
   </React.StrictMode>,
   document.getElementById('root')
 )
